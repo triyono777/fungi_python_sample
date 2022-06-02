7009e9e5dbd2d5dac198fb516208e4c6d5cfe0c6
Triyono <yono.tri@gmail.com>
2022-06-02 14:55:23 +0700

    penambahan fungsi return

---
+++ fungsi_dengan_return.py
@@ -0,0 +1,20 @@
+# fungsi dengan return
+
+
+def ucapan_selamat():
+    print('halo selamat siang')
+
+
+selamat = ucapan_selamat()
+
+print(selamat)
+
+
+def penjumlahan(a, b):
+    hasil = a+b
+    return hasil
+
+
+hasil_jumlah = penjumlahan(2, 10)
+
+print(hasil_jumlah)
--- fungsi_parameter.py
+++ fungsi_parameter.py
@@ -2,6 +2,12 @@ def selamat(nama, waktu):
     print('selamat', waktu, nama, '......')
 
 
+def greeting(name='tri', time='pagi'):  # with default parameter
+    print('selamat', time, name, '......')
+
+
 selamat('budi', 'pagi')
 selamat('malam', 'andi')  # positional parameter
 selamat(waktu='malam', nama='andi')  # named parameter
+greeting()  # default parameter
+greeting('rudi', 'petang')  # default parameter
